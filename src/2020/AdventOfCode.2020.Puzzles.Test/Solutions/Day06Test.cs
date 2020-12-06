78404d12fd46319f6910fe4e9d9d2caf407f8ae6
Jamie Magee <jamie.magee@gmail.com>
2020-12-06 15:51:21 +0100

    Day 6

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day06Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day06Test : TestBase<Day06>
+    {
+        private const string Input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+        
+        [Fact]
+        public async Task Part1()
+        {
+            
+            Assert.Equal("11", await Solution.Part1Async(Input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            Assert.Equal("6", await Solution.Part2Async(Input));
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day06.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Custom Customs")]
+    public sealed class Day06 : SolutionBase
+    {
+        protected override string Part1(string input)
+        {
+            return ParseInput(input).Select(x => new string(
+                        x.Replace("\n", "")
+                            .Distinct()
+                            .ToArray()
+                    )
+                )
+                .Sum(x => x.Length)
+                .ToString();
+        }
+
+        protected override string Part2(string input)
+        {
+            return ParseInput(input).Select(x =>
+                    x.Split("\n").Aggregate((a, b) => new string(a.Intersect(b).ToArray()))
+                )
+                .Sum(x => x.Length)
+                .ToString();
+        }
+
+        private static IEnumerable<string> ParseInput(string input)
+        {
+            return input.Trim().Split("\n\n");
+        }
+    }
+}
