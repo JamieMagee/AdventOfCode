ed6b7a146785a10e77201a8cf9bb9ca1964457be
Jamie Magee <jamie.magee@gmail.com>
2020-12-08 10:44:55 +0100

    Day 8

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day08Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day08Test : TestBase<Day08>
+    {
+        private const string Input = @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+        [Fact]
+        public async Task Part1()
+        {
+            Assert.Equal("5", await Solution.Part1Async(Input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            Assert.Equal("8", await Solution.Part2Async(Input));
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day08.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Handheld Halting")]
+    public sealed class Day08 : SolutionBase
+    {
+        protected override string Part1(string input)
+        {
+            var instructions = ParseInput(input);
+            var (_, acc) = RunProgram(instructions);
+            return acc.ToString();
+        }
+
+        protected override string Part2(string input)
+        {
+            var instructions = ParseInput(input);
+            var res = 0;
+            foreach (var t in instructions)
+            {
+                var command = t.Command;
+                t.Command = command switch
+                {
+                    "jmp" => "nop",
+                    "nop" => "jmp",
+                    _ => command
+                };
+                var (halts, acc) = RunProgram(instructions);
+                if (halts)
+                {
+                    res = acc;
+                    break;
+                }
+                t.Command = command;
+            }
+
+            return res.ToString();
+        }
+
+        private static (bool halts, int acc) RunProgram(IList<Instruction> instructions)
+        {
+            var seen = new HashSet<int>();
+            var acc = 0;
+            var pos = 0;
+            while (pos < instructions.Count)
+            {
+                if (seen.Contains(pos))
+                {
+                    return (false, acc);
+                }
+                seen.Add(pos);
+                switch (instructions[pos].Command)
+                {
+                    case "acc":
+                    {
+                        acc += instructions[pos].Argument;
+                        pos++;
+                        break;
+                    }
+                    case "jmp":
+                    {
+                        pos += instructions[pos].Argument;
+                        break;
+                    }
+                    case "nop":
+                    {
+                        pos++;
+                        break;
+                    }
+                }
+            }
+            return (true, acc);
+        }
+
+        private static IList<Instruction> ParseInput(string input)
+        {
+            return GetLines(input).Select(x => x.Split(' '))
+                .Select(x => new Instruction {Command = x[0], Argument = int.Parse(x[1])})
+                .ToList();
+        }
+
+        private class Instruction
+        {
+            public string Command { get; set; }
+            public int Argument { get; set; }
+        }
+    }
+}
