c4350b5ac7021065e7ad48cf93650732943984c1
Jamie Magee <jamie.magee@gmail.com>
2020-12-10 13:46:21 +0100

    Day 10

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day10Test.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day10Test : TestBase<Day10>
+    {
+        
+        const string Input = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+        [Fact]
+        public async Task Part1()
+        {
+            
+            Assert.Equal("220", await Solution.Part1Async(Input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            Assert.Equal("19208", await Solution.Part2Async(Input));
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day10.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Core;
+using AdventOfCode.Core.Extensions;
+using MoreLinq;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Adapter Array")]
+    public sealed class Day10 : SolutionBase
+    {
+        protected override string Part1(string input)
+        {
+            var parsedInput = input.GetLines<int>().ToList();
+            parsedInput.Add(0);
+            parsedInput.Add(parsedInput.Max() + 3);
+            var differences = parsedInput
+                .OrderBy(x => x)
+                .Pairwise((a, b) => b - a)
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, y => y.Count());
+            return (differences[1] * differences[3]).ToString();
+        }
+
+        protected override string Part2(string input)
+        {
+            var parsedInput = input.GetLines<int>().ToList();
+            parsedInput.Add(0);
+            parsedInput.Add(parsedInput.Max() + 3);
+            parsedInput.Sort();
+            var memoize = new Dictionary<int, long>
+            {
+                [parsedInput.Count - 1] = 1
+            };
+            for (var k = parsedInput.Count - 2; k >= 0; k--)
+            {
+                var connections = 0L;
+                for (var connected = k + 1;
+                    connected < parsedInput.Count && parsedInput[connected] - parsedInput[k] <= 3;
+                    connected++)
+                {
+                    connections += memoize[connected];
+                }
+
+                memoize[k] = connections;
+            }
+
+            return memoize[0].ToString();
+        }
+    }
+}
