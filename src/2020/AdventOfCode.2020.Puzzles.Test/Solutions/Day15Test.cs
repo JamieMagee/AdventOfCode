916463b6fbeedf381c4b2a9df5038de22026b202
Jamie Magee <jamie.magee@gmail.com>
2020-12-16 14:33:01 +0100

    Day 15

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day15Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day15Test : TestBase<Day15>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var testCases = new List<(string input, string expected)>
+            {
+                ("0,3,6", "436"),
+                ("1,3,2", "1"),
+                ("2,1,3", "10"),
+                ("1,2,3", "27"),
+                ("2,3,1", "78"),
+                ("3,2,1", "438"),
+                ("3,1,2", "1836")
+            };
+            foreach (var (input, expected) in testCases)
+            {
+                Assert.Equal(expected, await Solution.Part1Async(input));
+            }
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var testCases = new List<(string input, string expected)>
+            {
+                ("0,3,6", "175594"),
+                ("1,3,2", "2578"),
+                ("2,1,3", "3544142"),
+                ("1,2,3", "261214"),
+                ("2,3,1", "6895259"),
+                ("3,2,1", "18"),
+                ("3,1,2", "362")
+            };
+            foreach (var (input, expected) in testCases)
+            {
+                Assert.Equal(expected, await Solution.Part2Async(input));
+            }
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day15.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Rambunctious Recitation")]
+    public sealed class Day15 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        {
+            var numbers = input.Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            return await Solve(numbers, 2020);
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            var numbers = input.Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            return await Solve(numbers, 30_000_000);
+        }
+
+        private async Task<string> Solve(IReadOnlyList<int> input, int number)
+        {
+            var spoken = new int[number];
+            Array.Fill(spoken, -1);
+
+            var turn = 1;
+            for (; turn < input.Count + 1; turn++)
+            {
+                spoken[input[turn - 1]] = turn;
+            }
+
+            var currentNumber = 0;
+            for (; turn < number; turn++)
+            {
+                if (IsUpdateProgressNeeded())
+                {
+                    await UpdateProgressAsync(turn, number);
+                }
+
+                var prevTime = spoken[currentNumber];
+                spoken[currentNumber] = turn;
+                currentNumber = prevTime != -1 ? turn - prevTime : 0;
+            }
+
+            return currentNumber.ToString();
+        }
+    }
+}
