90532a1ea2e09bc47291a9d09f52bd27e245b499
Jamie Magee <jamie.magee@gmail.com>
2020-12-18 09:36:23 +0100

    Day 16

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day16Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day16Test : TestBase<Day16>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            const string input = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+            Assert.Equal("71", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            const string input = @"class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9";
+            Solution.FieldFilter = s => s == "row" || s == "seat";
+            Assert.Equal("143", await Solution.Part2Async(input));
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day16.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+using MoreLinq;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Ticket Translation")]
+    public sealed class Day16 : SolutionBase
+    {
+        private readonly Regex _rulesRegex = new(@"(?<name>(\w+\s?)+):( (?<lower>\d+)\-(?<upper>\d+)(?: or)?){2}");
+        private readonly Regex _ticketRegex = new(@"^((?:\d,?)+)$", RegexOptions.Multiline);
+        public Func<string, bool> FieldFilter = s => s.StartsWith("departure"); 
+
+        protected override string Part1(string input)
+        {
+            var ticketMatches = _ticketRegex.Matches(input);
+            var nearbyTickets = ticketMatches.ToArray()[1..].Select(l => l.Value.Split(',').Select(int.Parse));
+
+            var rulesMatches = _rulesRegex.Matches(input);
+            var rules = rulesMatches.Select(m => (name: m.Groups["name"].Value, validators:
+                m.Groups["lower"].Captures.EquiZip<Capture, Capture, Func<int, bool>>(m.Groups["upper"].Captures,
+                    (l, u) => n => n >= int.Parse(l.Value) && n <= int.Parse(u.Value))));
+
+            return nearbyTickets.Aggregate(0,
+                    (current, nearbyTicket) => current + nearbyTicket
+                        .Where(n => !rules.Any(r => r.validators.Any(s => s(n))))
+                        .Aggregate(0, (x, y) => x + y))
+                .ToString();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            var ticketMatches = _ticketRegex.Matches(input);
+            var myTicket = ticketMatches[0].Value.Split(',').Select(int.Parse).ToArray();
+            var nearbyTickets = ticketMatches.ToArray()[1..]
+                .Select(l => l.Value.Split(',')
+                    .Select(int.Parse)
+                    .ToArray()
+                );
+
+            var rulesMatches = _rulesRegex.Matches(input);
+            var rules = rulesMatches.Select(m => (
+                    name: m.Groups["name"].Value,
+                    validators: m.Groups["lower"].Captures.EquiZip<Capture, Capture, Func<int, bool>>(
+                        m.Groups["upper"].Captures,
+                        (l, u) => n => n >= int.Parse(l.Value) && n <= int.Parse(u.Value))))
+                .ToArray();
+
+            var validTickets = nearbyTickets.Where(nearbyTicket =>
+                    nearbyTicket.All(n => rules.Any(r => r.validators.Any(s => s(n)))))
+                .ToArray();
+
+            var candidates = rules.Select(r =>
+                    (r.name,
+                        indexes: Enumerable.Range(0, validTickets[0].Length)
+                            .Where(i => validTickets.All(t => r.validators.Any(v => v(t[i]))))
+                            .ToList()))
+                .ToList();
+
+            var map = new List<(string name, int index)>();
+            while (map.Count < rules.Length)
+            {
+                if (IsUpdateProgressNeeded())
+                {
+                    await UpdateProgressAsync(map.Count, rules.Length);
+                }
+
+                var (name, ints) = candidates.First(c => c.indexes.Count == 1);
+                map.Add((name, ints[0]));
+                candidates.RemoveAll(c => c.name == name);
+                foreach (var (_, indexes) in candidates)
+                {
+                    indexes.Remove(ints[0]);
+                }
+            }
+
+            return map
+                .Where(r => FieldFilter(r.name))
+                .Select(r => myTicket[r.index])
+                .Aggregate(1L, (l, r) => l * r)
+                .ToString();
+        }
+    }
+}
