085a8b0fbd8b88134a54baeab6aa005ea413c447
Jamie Magee <jamie.magee@gmail.com>
2020-12-18 14:22:17 +0100

    Day 17

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day17Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day17Test : TestBase<Day17>
+    {
+        private const string Input = @".#.
+..#
+###";
+        [Fact]
+        public async Task Part1()
+        {
+            Assert.Equal("112", await Solution.Part1Async(Input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            Assert.Equal("848", await Solution.Part2Async(Input));
+        }
+    }
+}
