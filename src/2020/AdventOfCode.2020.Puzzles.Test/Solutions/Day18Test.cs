e4e8d0868998bb7e3397fb125c132cd41a9630c5
Jamie Magee <jamie.magee@gmail.com>
2020-12-22 13:34:24 +0100

    Day 18

---
+++ src/2020/AdventOfCode.2020.Puzzles.Test/Solutions/Day18Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdventOfCode._2020.Puzzles.Solutions;
+using AdventOfCode.Core.Test;
+using Xunit;
+
+namespace AdventOfCode._2020.Puzzles.Test.Solutions
+{
+    public sealed class Day18Test : TestBase<Day18>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var testCases = new List<(string input, string expected)>
+            {
+                ("1 + 2 * 3 + 4 * 5 + 6", "71"),
+                ("1 + (2 * 3) + (4 * (5 + 6))", "51"),
+                ("2 * 3 + (4 * 5)", "26"),
+                ("5 + (8 * 3 + 9 + 3 * 4 * 3)", "437"),
+                ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "12240"),
+                ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "13632")
+            };
+            foreach (var (input, expected) in testCases)
+            {
+                Assert.Equal(expected, await Solution.Part1Async(input));
+            }
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var testCases = new List<(string input, string expected)>
+            {
+                ("1 + 2 * 3 + 4 * 5 + 6", "231"),
+                ("1 + (2 * 3) + (4 * (5 + 6))", "51"),
+                ("2 * 3 + (4 * 5)", "46"),
+                ("5 + (8 * 3 + 9 + 3 * 4 * 3)", "1445"),
+                ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "669060"),
+                ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "23340")
+            };
+            foreach (var (input, expected) in testCases)
+            {
+                Assert.Equal(expected, await Solution.Part2Async(input));
+            }
+        }
+    }
+}
---
+++ src/2020/AdventOfCode.2020.Puzzles/Solutions/Day18.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using AdventOfCode.Core;
+using AdventOfCode.Core.Extensions;
+
+namespace AdventOfCode._2020.Puzzles.Solutions
+{
+    [Puzzle("Operation Order")]
+    public sealed class Day18 : SolutionBase
+    {
+        private const char Space = ' ';
+        private const char Add = '+';
+        private const char Multiply = '*';
+        private const char OpenBracket = '(';
+        private const char CloseBracket = ')';
+
+        protected override string Part1(string input)
+        {
+            return input.GetLines<string>()
+                .Select(line => ReversePolishNotation(line, false))
+                .Sum()
+                .ToString(CultureInfo.InvariantCulture);
+        }
+
+        protected override string Part2(string input)
+        {
+            return input.GetLines<string>()
+                .Select(line => ReversePolishNotation(line, true))
+                .Sum()
+                .ToString(CultureInfo.InvariantCulture);
+        }
+        
+        private static double ReversePolishNotation(string line, bool part2)
+        {
+            var values = new Stack<double>();
+            var ops = new Stack<char>();
+            ops.Push(OpenBracket);
+            
+            foreach (var c in line)
+            {
+                switch (c)
+                {
+                    case Space:
+                        break;
+                    case Add:
+                        (ops, values) = Calculate(ops, values, part2);
+                        ops.Push(Add);
+                        break;
+                    case Multiply:
+                        (ops, values) = Calculate(ops, values);
+                        ops.Push(c);
+                        break;
+                    case OpenBracket:
+                        ops.Push(c);
+                        break;
+                    case CloseBracket:
+                        (ops, values) = Calculate(ops, values);
+                        ops.Pop();
+                        break;
+                    default:
+                        values.Push(char.GetNumericValue(c));
+                        break;
+                }
+            }
+            (_, values) = Calculate(ops, values);
+            return values.Single();
+        }
+
+        private static (Stack<char> ops, Stack<double> values) Calculate(Stack<char> ops, Stack<double> values, bool part2 = false)
+        {
+            while (!(ops.Peek() == OpenBracket || part2 && ops.Peek() == Multiply))
+            {
+                switch (ops.Pop())
+                {
+                    case Add:
+                        values.Push(values.Pop() + values.Pop());
+                        break;
+                    case Multiply:
+                        values.Push(values.Pop() * values.Pop());
+                        break;
+                }
+            }
+
+            return (ops, values);
+        }
+    }
+}
